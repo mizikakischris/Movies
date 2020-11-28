8ece5da4331fb53f672bed555c9388fa682e2669
Chris Mizikakis <cmizikakis@gmail.com>
2020-11-29 01:53:36 +0200

    Create movie model repository

---
+++ Movie.Api/Repository/IRepository/IMovieModelRepository.cs
@@ -0,0 +1,17 @@
+﻿using Movie.Api.Models;
+using System.Collections.Generic;
+
+namespace Movie.Api.Repository.IRepository
+{
+    public interface IMovieModelRepository
+    {
+        ICollection<MovieModel> GetMovies();
+        MovieModel GetMovieModel(int movieId);
+        bool MovieModelExists(string name);
+        bool MovieModelExists(int id);
+        bool CreateMovieModel(MovieModel movie);
+        bool UpdateMovieModel(MovieModel movie);
+        bool DeleteMovieModel(MovieModel movie);
+        bool Save();
+    }
+}
