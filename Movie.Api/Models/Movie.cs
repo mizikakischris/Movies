64d9b69a4122494da5c4bc59c3612dbc03edab34
Chris Mizikakis <cmizikakis@gmail.com>
2020-11-29 00:47:59 +0200

    Create movie model

---
+++ Movie.Api/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Api.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate{ get; set; }
+
+        [Required]
+        [Display(Name = "Box Office")]
+        public decimal BoxOffice { get; set; }
+
+        public byte[] Picture { get; set; }
+    }
+}
