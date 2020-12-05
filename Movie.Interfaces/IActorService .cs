15bf9fc8e3eb187e16a464770be91248da2c5c67
Chris Mizikakis <cmizikakis@gmail.com>
2020-12-06 00:39:11 +0200

    Implement Actor service

---
+++ Movie.Interfaces/IActorService .cs
@@ -0,0 +1,21 @@
+﻿using Movie.Types.Dtos;
+using Movie.Types.Models;
+using Movie.Types.Responses;
+using System.Collections.Generic;
+
+namespace Movie.Interfaces
+{
+    public interface IActorService
+    {
+        List<ActorDto> GetActors();
+        ActorDto GetActor(int actorId);
+        bool ActorExists(string actorName);
+        bool ActorExists(int actorId);
+        Actor CreateActor(ActorDto actor);
+        bool UpdateActor(Actor actor);
+        bool DeleteActor(Actor actor);
+        bool Save();
+
+        Actor GetTheActor(int movieId);
+    }
+}
---
+++ Movie.Services/ActorService.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Movie.Interfaces;
+using Movie.Types.Dtos;
+using Movie.Types.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Movie.Services
+{
+    public class ActorService : IActorService
+    {
+
+        private readonly IActorRepositoryService _repo;
+        private readonly IMapper _mapper;
+        public ActorService(IActorRepositoryService repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        public Actor CreateActor(ActorDto actorDto)
+        {
+            var actorObj = _mapper.Map<Actor>(actorDto);
+            if (!_repo.CreateActor(actorObj))
+            {
+                throw new Exception( $"Something went wrong when saving the record {actorObj.Name}");
+               
+            }
+            return actorObj;
+        }
+
+        public bool DeleteActor(Actor actor)
+        {
+            return _repo.DeleteActor(actor);
+        }
+
+        public ActorDto GetActor(int actorId)
+        {
+            var actor =  _repo.GetActor(actorId);
+            var actorDto = _mapper.Map<ActorDto>(actor);
+
+            return actorDto;
+        }
+
+        public Actor GetTheActor(int actorId)
+        {
+            var actor = _repo.GetActor(actorId);
+
+            return actor;
+        }
+
+        public List<ActorDto> GetActors()
+        {
+            var actorsList =  _repo.GetActors();
+            var actorDtos = new List<ActorDto>();
+            foreach (var actor in actorsList)
+            {
+                actorDtos.Add(_mapper.Map<ActorDto>(actor));
+            }
+
+            return actorDtos;
+        }
+
+
+        public bool ActorExists(string name)
+        {
+            return _repo.ActorExists(name);
+        }
+
+        public bool ActorExists(int id)
+        {
+            return _repo.ActorExists(id);
+        }
+
+        public bool Save()
+        {
+            return _repo.Save();
+        }
+
+        public bool UpdateActor(Actor actor)
+        {
+            return _repo.UpdateActor(actor);
+        }
+    }
+
+    
+}
