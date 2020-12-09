622ef433c6e04a6997fdc41fa675fda055af591f
Chris Mizikakis <cmizikakis@gmail.com>
2020-12-10 00:18:24 +0200

    Add IUserRepository

---
+++ Movie.Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Movie.Types.Models;
+
+namespace Movie.Interfaces
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        User Authenticate(string username, string password);
+        User Register(string username, string password);
+    }
+}
---
+++ Movie.Repository/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Movie.Interfaces;
+using Movie.Repository.Data;
+using Movie.Types.Models;
+using System;
+using System.Text;
+
+namespace Movie.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
