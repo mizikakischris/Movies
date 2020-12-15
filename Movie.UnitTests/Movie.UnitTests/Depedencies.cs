617415b40a73008cb8ce50ce786a90de0058f3a3
Chris Mizikakis <cmizikakis@gmail.com>
2020-12-15 02:27:12 +0200

    Add Unit test

---
+++ Movie.UnitTests/Movie.UnitTests/Depedencies.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie.Repository.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Movie.UnitTests
+{
+    public class Depedencies
+    {
+
+        string _connectionString;
+        public DbContextOptions<AppDbContext> Options { get; set; }
+        public void Setup()
+        {
+
+            var config = InitConfiguration();
+            _connectionString = config.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value;
+           
+            Options = GetOptions(_connectionString);
+
+        }
+   
+        private  DbContextOptions<AppDbContext> GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder<AppDbContext>(), connectionString).Options;
+        }
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+            return config;
+        }
+    }
+
+}
---
+++ Movie.UnitTests/Movie.UnitTests/MovieTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Movie.Repository;
+using Movie.Repository.Data;
+
+namespace Movie.UnitTests
+{
+    [TestClass]
+    public class MovieTest
+    {
+        private readonly Depedencies _depedencies;
+
+        public MovieTest()
+        {
+            _depedencies = new Depedencies();
+            _depedencies.Setup();
+
+        }
+        [TestMethod]
+        public void GetAllMovies_Test()
+        {
+            using (var appDbContext = new AppDbContext(_depedencies.Options)) // <-- Pass the options here
+            {
+                MovieModelRepository modelRepo = new MovieModelRepository(appDbContext);
+                var movies = modelRepo.GetMovies();
+
+                Assert.IsNotNull(movies);
+                Assert.IsTrue(movies.Count > 0);
+            }
+
+        }
+
+    }
+}
